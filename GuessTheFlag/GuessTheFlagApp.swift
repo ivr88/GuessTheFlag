4cde9ad46f126a42f2c9aeedd0225d80d1275429
ivr88 <114075652+ivr88@users.noreply.github.com>
2023-11-13 16:35:55 +0300

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Вадим Исламов on 13.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
